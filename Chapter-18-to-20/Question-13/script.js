86e4fa831dd5bdd017328bdee86a73a4f8d28d62
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-14 03:41:09 +0500

    Improve formatting for Question 13: Student score table

--- Chapter-18-to-20/Question-13/script.js
+++ Chapter-18-to-20/Question-13/script.js
@@ -3,18 +3,20 @@ var scores = [58, 73, 89, 90];
 
 document.write("<table border='1px'>");
 
-document.write(`<tr>
+document.write(`
+<tr>
     <td>Students</td>
     <td>Scores</td>
-</tr>`);
+</tr>
+`);
 
 for (let i = 0; i < students.length; i++) {
-    document.write("<tr>");
-    for (let j = 0; j < 1; j++) {
-        document.write(`<td>${students[i]}`);
-        document.write(`<td>${scores[i]}`);
-    }
-    document.write("</tr>");
+    document.write(`
+        <tr>
+            <td>${students[i]}</td>
+            <td>${scores[i]}</td>
+        </tr>
+    `);
 }
 
 document.write("</table>");
